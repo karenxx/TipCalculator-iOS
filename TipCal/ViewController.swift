87ccc382e64ef354399bad2ef2d6f6d2475b8786
XIXIAN GUO <xixian@XIXIANs-MacBook-Pro.local>
2017-01-04 17:45:42 -0800

    add split bill

--- TipCal/ViewController.swift
+++ TipCal/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billVal: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipChoice: UISegmentedControl!
+    
+    @IBOutlet weak var numberField: UITextField!
+    @IBOutlet weak var youpayField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        youpayField.isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +30,37 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let tipPercentage = [0.15, 0.20, 0.25]
+        
+        let bill = Double(billVal.text!) ?? 0  //return 0 if nill
+        let tip = bill * tipPercentage[tipChoice.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+    @IBAction func calEachPay(_ sender: AnyObject) {
+        youpayField.isHidden = false
+        
+        let number = Double(numberField.text!) ?? 1
+        
+        let tipPercentage = [0.15, 0.20, 0.25]
+        let bill = Double(billVal.text!) ?? 0
+        let tip = bill * tipPercentage[tipChoice.selectedSegmentIndex]
+        let total = bill + tip
+        
+        if(number == 0){
+            youpayField.text = String(format: "$%.2f", 0.00)
+        }
+        else {
+            let onePay = Double(total / number)
+            youpayField.text = "Each pay " + String(format: "$%.2f", onePay)
+        }
+    }
 }
 
